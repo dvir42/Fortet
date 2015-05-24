6dbf4d40a31d4139f0cbbbf929da111dfe71705b
dvir42 <dviros7@gmail.com>
2015-05-24 11:58:48 +0300

    Added JPrince and improved graphics

---
+++ src/stuff/JPrince.java
@@ -0,0 +1,39 @@
+package stuff;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+public class JPrince extends JLabel {
+
+	private static final long serialVersionUID = 7139877797953720431L;
+
+	private static final int rows = 4, cols = 4;
+	private static final ImageIcon[][] pics = new ImageIcon[rows][cols];
+
+	static {
+		ImageIcon ic = new ImageIcon("imgs/prince.png");
+		int imgHeight = ic.getIconHeight();
+		int imgWidth = ic.getIconWidth();
+		BufferedImage bi = new BufferedImage(imgWidth, imgHeight,
+				BufferedImage.TYPE_INT_ARGB);
+		Graphics g = bi.getGraphics();
+		g.drawImage(ic.getImage(), 0, 0, null);
+		int px = 0, py = 0, w = imgWidth / cols, h = imgHeight / rows;
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < cols; j++) {
+				pics[i][j] = new ImageIcon(bi.getSubimage(px, py, w, h));
+				px += w;
+			}
+			py += h;
+			px = 0;
+		}
+	}
+
+	public JPrince() {
+		super(pics[0][0]);
+	}
+
+}
--- src/stuff/Maze.java
+++ src/stuff/Maze.java
@@ -1,27 +1,38 @@
 package stuff;
+
 import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.JLayeredPane;
 import javax.swing.JTable;
+import javax.swing.border.EtchedBorder;
 
-public class Maze extends JPanel {
-
-	private static final long serialVersionUID = -7550760796122646082L;
+public class Maze {
 
 	public static final int WIDTH = 10;
 	public static final int HEIGHT = 10;
-	public static final int ROW_HEIGHT = 50;
+	public static final int ROW_HEIGHT = 60;
 
 	private final JTable table;
+	private final JPrince prince;
+	private final JLayeredPane pane;
 
 	public Maze() {
-		table = new JTable(new MazeTableModel());
-		table.setEnabled(false);
-		table.setRowHeight(ROW_HEIGHT);
 		JFrame frame = new JFrame("Maze");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.add(this);
-		add(table);
-		frame.pack();
+		frame.setSize((WIDTH + 1) * ROW_HEIGHT, (HEIGHT + 1) * ROW_HEIGHT);
+		pane = new JLayeredPane();
+		prince = new JPrince();
+		pane.setBorder(new EtchedBorder());
+		table = new JTable(new MazeTableModel());
+		table.setEnabled(false);
+		table.setBorder(new EtchedBorder());
+		table.setRowHeight(ROW_HEIGHT);
+		table.setBounds(ROW_HEIGHT / 3 + ROW_HEIGHT / WIDTH, ROW_HEIGHT / 4,
+				WIDTH * ROW_HEIGHT, HEIGHT * ROW_HEIGHT);
+		frame.getContentPane().add(pane);
+		pane.add(table, JLayeredPane.DEFAULT_LAYER);
+		pane.add(prince, JLayeredPane.PALETTE_LAYER);
+		prince.setSize(ROW_HEIGHT, ROW_HEIGHT);
+		prince.setLocation(ROW_HEIGHT / 3 + ROW_HEIGHT / WIDTH, ROW_HEIGHT / 4);
 		frame.setVisible(true);
 	}
 
