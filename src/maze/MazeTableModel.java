25a7d77ae4313971f0f2889c22c118b1df0dc343
dvir42 <dviros7@gmail.com>
2015-06-08 17:13:56 +0300

    Chooses a maze instead of using a default one

--- src/maze/MazeTableModel.java
+++ src/maze/MazeTableModel.java
@@ -4,12 +4,12 @@ import jStuff.JDragon;
 import jStuff.JPrincess;
 import jStuff.JTreasure;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.List;
 
 import javax.swing.ImageIcon;
+import javax.swing.JFileChooser;
 import javax.swing.table.AbstractTableModel;
 
 /**
@@ -22,10 +22,10 @@ public class MazeTableModel extends AbstractTableModel {
 
 	private static final long serialVersionUID = -4347071534736678347L;
 
-	private static final ImageIcon PRINCESS = JPrincess.cry();
-	private static final ImageIcon TREASURE = JTreasure.closed();
-	private static final ImageIcon BLOCKER = new ImageIcon("imgs/blocker.png");
-	private static final ImageIcon DRAGON = JDragon.sleeping();
+	public static final ImageIcon PRINCESS = JPrincess.cry();
+	public static final ImageIcon TREASURE = JTreasure.closed();
+	public static final ImageIcon BLOCKER = new ImageIcon("imgs/blocker.png");
+	public static final ImageIcon DRAGON = JDragon.sleeping();
 
 	private static final Object[][] data;
 
@@ -60,40 +60,46 @@ public class MazeTableModel extends AbstractTableModel {
 	static {
 		data = new Object[Maze.HEIGHT][Maze.WIDTH];
 		try {
-			List<String> temp = Files.readAllLines(new File("maze").toPath());
-			for (int i = 0; i < Maze.HEIGHT; i++)
-				for (int j = 0; j < Maze.WIDTH; j++)
-					switch (Integer.parseInt(temp.get(i).split(" ")[j])) {
-					case 1:
-						data[i][j] = BLOCKER;
-						Maze.blockers[i][j] = true;
-						break;
-					case 2:
-						data[i][j] = PRINCESS;
-						Maze.princessI = i;
-						Maze.princessJ = j;
-						break;
-					case 3:
-						data[i][j] = TREASURE;
-						Maze.treasures[i][j] = new JTreasure(false);
-						break;
-					case 4:
-						data[i][j] = TREASURE;
-						Maze.treasures[i][j] = new JTreasure(true);
-						break;
-					case 5:
-						Maze.princeI = i;
-						Maze.princeJ = j;
-						break;
-					case 6:
-						data[i][j] = DRAGON;
-						Maze.startingDragonI = i;
-						Maze.startingDragonJ = j;
-						break;
-					default:
-						data[i][j] = null;
-						break;
-					}
+			JFileChooser chooser = new JFileChooser(".");
+			chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+			if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+				List<String> temp = Files.readAllLines(chooser
+						.getSelectedFile().toPath());
+				for (int i = 0; i < Maze.HEIGHT; i++)
+					for (int j = 0; j < Maze.WIDTH; j++)
+						switch (Integer.parseInt(temp.get(i).split(" ")[j])) {
+						case 1:
+							data[i][j] = BLOCKER;
+							Maze.blockers[i][j] = true;
+							break;
+						case 2:
+							data[i][j] = PRINCESS;
+							Maze.princessI = i;
+							Maze.princessJ = j;
+							break;
+						case 3:
+							data[i][j] = TREASURE;
+							Maze.treasures[i][j] = new JTreasure(false);
+							break;
+						case 4:
+							data[i][j] = TREASURE;
+							Maze.treasures[i][j] = new JTreasure(true);
+							break;
+						case 5:
+							Maze.princeI = i;
+							Maze.princeJ = j;
+							break;
+						case 6:
+							data[i][j] = DRAGON;
+							Maze.startingDragonI = i;
+							Maze.startingDragonJ = j;
+							break;
+						default:
+							data[i][j] = null;
+							break;
+						}
+			} else
+				System.exit(0);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
